fa383a6e8ab4058b33830b54045949997520a6c3
Dexamenus <abdan.annur123@gmail.com>
2026-04-08 15:03:57 +0700

    first

---
+++ src/main/java/com/mycompany/pemkom2/PEMKOM2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pemkom2;
+
+/**
+ *
+ * @author HP VICTUS
+ */
+public class PEMKOM2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
